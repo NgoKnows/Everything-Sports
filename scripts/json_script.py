3ff3223c82ad21c80f8a7728ecd3018b62be0782
Your Name <mhandrea@uw.edu>
2014-12-02 10:30:23 -0800

    udpated json_script

--- scripts/json_script.py
+++ scripts/json_script.py
@@ -26,28 +26,37 @@ opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 
-def main(keyword='source'):
+def main(keyword='baseball'):
     """Pulls all links and headlines in RSS feed"""
 
+    articles = []
+    article = dict()
     for url in urls:
         try:
             page = urls[url]
             source_code = opener.open(page).read()
-
+            print source_code
             try:
-                for code in source_code:
-                    titles = re.findall(r'<title>(.*?)</title>', source_code)
-                    links = re.findall(r'<link>(.*?)</link>', source_code)
+                items = re.findall(r'<item>(.*?)</item>', source_code)
+                print type(items)
+                for item in items:
+                    # print item
+                    # print re.findall(r'<dc:creator>(.*?)</dc:creator>', item)
+                    article['creator'] = re.DOTALL(r'<dc:creator>(.*?)</dc:creator>', item)
+                    article['title'] = re.DOTALL(r'<title>(.*?)</title>', item)
+                    article['description'] = re.DOTALL(r'<description>(.*?)</description>', item)
+                    article['date'] = re.DOTALL(r'<pubDate>(.*?)</pubDate>', item)
+                    article['link'] = re.DOTALL(r'<link>(.*?)</link>', item)
+                    articles.append(article)
 
-                    for title in titles:
-                        if keyword in title:
-                            print title
-                    
             except Exception, e:
                 print str(e)
         
         except Exception,e:
             print str(e), url
             pass
+
+    print articles
+    return articles
 
 main()
