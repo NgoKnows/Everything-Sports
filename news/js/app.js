973e5570bb80d407529cb68f44e6c110351b6431
Michael Andrea <mhandrea@uw.edu>
2014-12-02 21:26:45 -0800

    moving to my mac, python script wont run on window self hosted server

--- news/js/app.js
+++ news/js/app.js
@@ -6,10 +6,14 @@
 
 angular.module('sports_news', [])
 	.controller('ArticleController', function($scope) {
-		console.log('here');
-		//$scope.articles = find_articles(keywords, $scope);
-		$scope.articles = get_articles();	
-		console.log('here');
+		$scope.articals = get_articles();
+        console.log('here');
+        console.log($scope.articles[0]);
+        // $scope.sort_articals = function(searchString) {
+        //     var keywords = keyword_string.split(' ');
+        //     $scope.articals = find_articles(keywords, $scope);
+        // };
+		// console.log('here');
 	});
 
 function get_articles() {
@@ -18,23 +22,28 @@ function get_articles() {
         .done(function(data) {
         	data = JSON.parse(data);
             data.forEach(function(article) {
-            	article['hidden'] = true;
+            	article['hidden'] = false;
                 articles.push(article);
-            })
-        })
-    return articles;
+                // console.log(article);
+            });
+            return articles;
+           //console.log(articles[2]);
+        });
+    
 }
 
 // function find_articles(keywords, $scope) {
-// 	var articles = get_articles();
 // 	$scope.articles.forEach(function(article) {
 // 		keywords.forEach(function(search_word) {
 // 			if (search_word in article['title'] || search_word in article['description'] || search_word in article['link'] || search_word in article['url']) {
 // 				article['hidden'] = false;
 // 				console.log('contains keyword, unhidden');
 // 			}
+//             else {
+//                 article['hidden'] = true;
+//             }
 // 		})
 // 	});
-// 	console.log(articles);
+// 	// console.log(articles);
 // 	return articles;
 // }
