56e77858c55ce8c034fe0b3509461b6b8f1176b1
Alex Ngo <alex_ngo@live.com>
2014-12-02 22:52:19 -0800

    added sorting, and removable categories (yay)

--- Fantasy/app.js
+++ Fantasy/app.js
@@ -9,13 +9,14 @@ var playerList;
 angular.module('FantasyTeam', [])
     .controller('TeamController', function ($scope) {
         $scope.players = playerList;
-        $scope.sortCat = 'pos';
+        $scope.sortCat = 'POS';
+
         $scope.statCats = ["POS", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA",
                      "2P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL",
                      "BLK", "TOV", "PF", "PTS"];
+        $scope.hiddenCats = ["2P", "2PA", "3P", "3PA"];
         $scope.refreshPlayerList = function () {
             getTeam();
-            console.log(playerList);
             $scope.players = playerList;
         };
         $scope.sortBy = function (sortCat) {
@@ -26,102 +27,54 @@ angular.module('FantasyTeam', [])
                 $scope.sortCat = sortCat;
                 $scope.sortReverse = false;
             }
+            console.log($scope.sortCat);
         }
         $scope.hoverOverCol = function () {
-            console.log(this.$index);
             $scope.curCol = this.$index;
         }
         $scope.hoverOverRow = function () {
             $scope.curRow = this.$index;
         }
         $scope.deletePlayer = function () {
-            console.log(this.$index);
             playerList.splice(this.$index, this.$index + 1);
             $scope.players = playerList;
         }
+        $scope.getSortedValue = function (player) {
+            if ($scope.players) {
+                if ($scope.sortCat == "POS") {
+                    return $.inArray(player.pos, ["C", "PF", "SF", "SG", "PG"]);
+                } else {
+                    var indexOfCat = $.inArray($scope.sortCat, $scope.statCats);
+                    return parseFloat(player.stats[indexOfCat]);
+                }
+            }
+        }
+        $scope.toggleHide = function (cat) {
+            var indexOfCat = $.inArray(cat, $scope.hiddenCats);
+            if (indexOfCat != -1) {
+                $scope.hiddenCats.splice(indexOfCat, indexOfCat + 1);
+            } else {
+                $scope.hiddenCats.push(cat);
+            }
+        }
+        $scope.isHidden = function (cat) {
+            var indexOfCat = $.inArray(cat, $scope.hiddenCats);
+            if (indexOfCat != -1) {
+                console.log(true);
+                return true;
+            } else {
+                console.log(false);
+                return false;
+            }
+
+        }
+        $scope.hiddenStat = function(index){
+            var cat = $scope.statCats[index];
+            if($.inArray(cat, $scope.hiddenCats) != -1){
+                return true;
+            }else{
+                return false;
+            }
+        };
+
     });
-
-
-
-
-/*
-$("table").delegate('td','mouseover mouseleave', function(e) {
-    if (e.type == 'mouseover') {
-      $(this).parent().addClass("hover");
-      $("colgroup").eq($(this).index()).addClass("hover");
-    }
-    else {
-      $(this).parent().removeClass("hover");
-      $("colgroup").eq($(this).index()).removeClass("hover");
-    }
-});*/
-
-function getTeam() {
-    var team = [];
-    var playerString = $('#team').val().replace(/\,/g, ' ');
-    console.log(playerString);
-    var playerArray = playerString.split(/\s+/);
-    for (var i = 0; i < playerArray.length - 1; i++) {
-        if (playerArray[i].substring(playerArray[i].length - 1, playerArray[i].length) == '.' && playerArray[i + 1].substring(playerArray[i + 1].length - 1, playerArray[i + 1].length) == '.') {
-            playerArray[i + 1] = playerArray[i].toUpperCase() + playerArray[i + 1].toUpperCase();
-            i = i + 1;
-        }
-
-        var playerName = getPlayerName(playerArray[i], playerArray[i + 1]);
-        console.log('player to be checked: ' + playerName);
-        if (checkIfPlayer(playerName)) {
-            team.push(playerName);
-        }
-    }
-    playerList = getPlayerList(team);
-    console.log(playerList);
-};
-
-
-function getPlayerName(firstName, lastName) {
-    var playerFirstName = firstName.substring(0, 1).toUpperCase() +
-        firstName.substring(1, firstName.length);
-    var playerLastName = lastName.substring(0, 1).toUpperCase() +
-        lastName.substring(1, lastName.length).toLowerCase();
-    var playerName = playerFirstName + ' ' + playerLastName;
-    return playerName;
-}
-
-function checkIfPlayer(playerName) {
-    console.log(playerDict.hasOwnProperty(playerName));
-    if (playerDict.hasOwnProperty(playerName)) {
-        return true;
-    }
-    return false;
-}
-
-function getPlayerDict() {
-    var playerDict1 = {};
-    $.getJSON('../data/data.json')
-        .done(function (data) {
-            data.forEach(function (player) {
-                playerDict1[player.name] = player.stats;
-            })
-        });
-    return playerDict1;
-}
-
-function getPlayerList(playersOnTeam) {
-    var playerList1 = [];
-    playersOnTeam.forEach(function (player) {
-        var curPlayer = {};
-        var statCats = ["POS", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA",
-                     "2P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL",
-                     "BLK", "TOV", "PF", "PTS"];
-        var statistics = [];
-        statCats.forEach(function (stat) {
-            statistics.push(playerDict[player][stat]);
-        })
-        console.log(statistics);
-        curPlayer.name = player;
-        curPlayer.stats = statistics;
-        curPlayer.pos = playerDict[player]['POS'];
-        playerList1.push(curPlayer);
-    })
-    return playerList1;
-}
