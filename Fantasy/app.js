e82b672849e913dd370a2892372ea42baa60599f
Alex Ngo <alex_ngo@live.com>
2014-12-04 02:02:01 -0800

    added new controller for compare players

--- Fantasy/app.js
+++ Fantasy/app.js
@@ -7,11 +7,10 @@ var playerDict = getPlayerDict();
 var playerList;
 var dataListPlayers = getDataList();
 
-angular.module('FantasyTeam', [])
+angular.module('Fantasy', [])
     .controller('TeamController', function ($scope) {
         $scope.players = [];
         $scope.sortCat = 'POS';
-        $scope.totals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         $scope.dataListPlayers = getDataList();
         $scope.statCats = ["POS", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA",
                      "2P%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL",
@@ -99,7 +98,7 @@ angular.module('FantasyTeam', [])
 
         }
         $scope.isHiddenTotal = function (index) {
-            var cat = $scope.statCats[index+1];
+            var cat = $scope.statCats[index + 1];
             var indexOfCat = $.inArray(cat, $scope.hiddenCats);
             if (indexOfCat != -1) {
                 return true;
@@ -125,5 +124,56 @@ angular.module('FantasyTeam', [])
 
         };
 
+
+    })
+    .controller("CompareController", function ($scope) {
+        $scope.players = [];
+        $scope.dataListPlayers = getDataList();
+        $scope.addPlayer = function () {
+            var player = [$("#playersearch").val()];
+            var newPlayer = getPlayerList(player);
+            if ($.inArray(player, playerList) == -1) {
+                $scope.players.push(newPlayer[0]);
+            }
+            console.log($scope.players);
+        }
+        $scope.search = function () {
+            if ($scope.dataListPlayers == undefined) {
+                $scope.dataListPlayers == dataListPlayers;
+            }
+        }
+        $scope.createGraph = function () {
+            console.log('hi');
+            var margin = {
+                    top: 20,
+                    right: 20,
+                    bottom: 30,
+                    left: 40
+                },
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+            var x0 = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+            var x1 = d3.scale.ordinal();
+            var y = d3.scale.linear()
+                .range([height, 0]);
+            var color = d3.scale.ordinal()
+                .range(["#097054", "#6599FF", "#FFDE00", "#FF9900"]);
+            var xAxis = d3.svg.axis()
+                .scale(x0)
+                .orient("bottom");
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left");
+            console.log(d3.select("#dfadf"));
+            console.log(d3.select("#graph"));
+
+            var svg = d3.select("#gddsaph").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        }
 
     });
