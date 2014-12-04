0a3ab31d184cad518b9a3c50cbac6a7f66399ffe
Michael Andrea <mhandrea@uw.edu>
2014-12-04 00:45:37 -0800

    search function fully done

--- news/js/app.js
+++ news/js/app.js
@@ -16,9 +16,9 @@ angular.module('SportsNews', [])
                 var words = search_string.split(' ');
                 var visible = true;
                 console.log(words);
-                words.forEach(function(term) {
-                    if (article.title.toLowerCase().indexOf(term) > -1) {
-
+                words.forEach(function(word) {
+                    if (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1) {
+                        visible = true;
                     }
                     else {
                         visible = false;
@@ -28,3 +28,30 @@ angular.module('SportsNews', [])
             };
         
     });
+
+
+// $scope.filters = "";
+        // $scope.search_string = [];
+        // $scope.setSearch = function(article, search_string) {
+        //     $scope.search_string = search_string.split(' ');
+        //     return (filter_by(article, $scope) > -1);
+
+        // };
+
+
+
+        // $scope.filter_by = function(article) {
+        //     return (article.title.toLowerCase().indexOf($scope.search_string[0]) > -1);
+        //     $scope.search_string.forEach(function(word) {
+        //         return (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1);
+        //     });
+        // };
+
+
+
+// function filtered(article, search_string) {
+//     console.log('here');
+    
+//     return (words.indexOf(article.title.toLowerCase()) > -1);
+//     //return (article.title.toLowerCase().indexOf(search_string) > -1 || article.description.toLowerCase().indexOf(search_string) > -1 || article.link.toLowerCase().indexOf(search_string) > -1);
+// }
