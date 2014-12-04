c0c4b704901d42e3481112e00ccf05c9c478ec62
Michael Andrea <mhandrea@uw.edu>
2014-12-04 08:06:08 -0800

    got 2 columns and optimized sort algorith

--- news/js/app.js
+++ news/js/app.js
@@ -11,58 +11,23 @@ angular.module('SportsNews', [])
                 $scope.articles = articles;
                 console.log('first');
             });
-            try {
-                $scope.filter_by = function(article, search_string) {
-                    console.log('here');
-                    var words;
-                    if (search_string) {
-                        words = search_string.toLowerCase().split(' ');
+            $scope.filter_by = function(article, search_string) {
+                console.log('here');
+                var words;
+                if (search_string) {
+                    words = search_string.toLowerCase().split(' ');
+                }
+                else {
+                    words = ['espn'];
+                }
+                var visible = false;
+                console.log(words);
+                words.forEach(function(word) {
+                    if (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1 || article.creator.toLowerCase().indexOf(word) > -1 || article.date.toLowerCase().indexOf(word) > -1 || article.url.toLowerCase().indexOf(word) > -1) {
+                        console.log("owdbwaiodb");
+                        visible = true;
                     }
-                    else {
-                        words = ['espn'];
-                    }
-                    var visible = false;
-                    console.log(words);
-                    words.forEach(function(word) {
-                        if (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1) {
-                            visible = true;
-                        }
-                        else {
-                            visible = false;
-                        }
-                        return visible;
-                    });
-                    return visible; // (words.indexOf(article.title.toLowerCase()) > -1)
-                };
-            } 
-            catch (err) {
-                console.log(err);
-            }
+                });
+                return visible; // (words.indexOf(article.title.toLowerCase()) > -1)
+            };
     });
-
-
-// $scope.filters = "";
-        // $scope.search_string = [];
-        // $scope.setSearch = function(article, search_string) {
-        //     $scope.search_string = search_string.split(' ');
-        //     return (filter_by(article, $scope) > -1);
-
-        // };
-
-
-
-        // $scope.filter_by = function(article) {
-        //     return (article.title.toLowerCase().indexOf($scope.search_string[0]) > -1);
-        //     $scope.search_string.forEach(function(word) {
-        //         return (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1);
-        //     });
-        // };
-
-
-
-// function filtered(article, search_string) {
-//     console.log('here');
-    
-//     return (words.indexOf(article.title.toLowerCase()) > -1);
-//     //return (article.title.toLowerCase().indexOf(search_string) > -1 || article.description.toLowerCase().indexOf(search_string) > -1 || article.link.toLowerCase().indexOf(search_string) > -1);
-// }
