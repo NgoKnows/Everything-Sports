10e51eb189d99d4a8c73439868fabb629e837554
Michael Andrea <mhandrea@uw.edu>
2014-12-03 21:40:33 -0800

    articles show up after button is clicked, but function runs before search_string and .articles is initiated

--- news/js/app.js
+++ news/js/app.js
@@ -10,13 +10,14 @@ angular.module('SportsNews', [])
        		.success(function(articles) {                
            		$scope.articles = articles;
         });
-       	$scope.search_string = 'espn';
+       	
+   		$scope.search_string = ' ';
     	$scope.filter_by = function(search_string) {
     		$scope.search_string = search_string.split(' ');
     		console.log($scope.search_string);
 			$scope.search_string.forEach(function(word) {
 				$scope.articles.forEach(function(article) {
-					return (article.title.indexOf(word) > -1 || article.description.indexOf(word) > -1 || article.link.indexOf(word) > -1);
+					return (article.title.toLowerCase().indexOf(word) > -1 || article.description.toLowerCase().indexOf(word) > -1 || article.link.toLowerCase().indexOf(word) > -1);
 				})
 			});
     	};
